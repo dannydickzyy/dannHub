7c3f24c0452c97ab7ce7e29daebc9d71bc04f3ea
dannydickzyy <notstrongenough17@gmail.com>
2025-09-03 22:37:11 +0800

    v1

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,17 @@
-row = input("Enter a Row: ")
-col = input("Enter a Column: ")
+while True:
+    row = input("Enter a Row: ")
+    col = input("Enter a Column: ")
+    search = input("Enter number to search: ")
 
-for x in range (1, (int(row) + 1)):
-    for y in range (1, (int(col) +1)):
-        print(f"{x*y:5}", end=" ")
-    print()
+    if int(col) <= 0 or int(row) <= 0:
+        print("Invalid Input")
+        break
+    
+    for x in range (1, (int(row) + 1)):
+        for y in range (1, (int(col) +1)):
+            prod = x * y
+            if prod == int(search):
+                print(f"[{prod}]", end=" ")
+            else:
+                print(f"{prod}", end=" ")
+        print()
